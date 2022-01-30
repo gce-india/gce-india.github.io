9c661ac8dbc3f84089c85ad6fc6b82f27a10e17f
Param Siddharth <contact@paramsid.com>
2022-01-30 12:32:43 +0530

    Initialize project
    Signed-off-by: Param Siddharth <contact@paramsid.com>

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+	<div className="App">
+		<header className="App-header">
+		<p>
+			Hello world!
+		</p>
+		</header>
+	</div>
+  );
+}
+
+export default App;
