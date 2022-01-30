827ddae142c2735f6cf56ebf952d2f2ad9c10019
Param Siddharth <contact@paramsid.com>
2022-01-31 02:01:08 +0530

    Add elements to homepage
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,7 @@
 import {
-	Container, Row, Col
+	Container,
+	Row, Col,
+	Button
 } from 'reactstrap';
 
 import './main.css';
@@ -11,7 +13,7 @@ const Main = () => {
 		<Row>
 			<Col className='text-center' >
 				<Col xs='6' md='4' lg='2' className='mx-auto my-4' >
-					<a href='/'><img src='/assets/logo.png' alt='Logo' className='w-100' /></a>
+					<a href='#'><img src='/assets/logo.png' alt='Logo' className='w-100' /></a>
 				</Col>
 				<Col className='levitating'>
 					<Greetings />
@@ -19,8 +21,28 @@ const Main = () => {
 			</Col>
 		</Row>
 		<Row className='text-center px-1' style={{ marginTop: `${5 * anim.charLen}em`, zIndex: 4 }}>
-			<Col>
+			<Col xs='12 mb-2'>
 				<h5>We are GitHub Campus Experts from India.</h5>
+			</Col>
+			<Col xs='12'>
+				<Row>
+					<Col className='col-12 col-md-2 offset-md-3 mb-1'>
+						<a 
+							href='https://apply.githubcampus.expert/'
+							target='_blank'
+							className='btn d-block w-100 btn-light' style={{
+							backgroundColor: '#DD5522',
+							borderColor: '#DD5522',
+							color: 'white'
+						}}><b>🚩 Apply</b></a>
+					</Col>
+					<Col className='col-12 col-md-2 mb-1'>
+						<a href='#' className='btn d-block w-100 btn-light text-black'><b>🎭 Discover</b></a>
+					</Col>
+					<Col className='col-12 col-md-2 mb-1'>
+						<a href='#' className='btn d-block w-100 btn-success'><b>📨 Contact</b></a>
+					</Col>
+				</Row>
 			</Col>
 		</Row>
 	</Container>;
