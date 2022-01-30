b06fc571eb132c634e611486c48d4fd174933f76
Param Siddharth <contact@paramsid.com>
2022-01-30 13:15:04 +0530

    Configure build
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [
+		react(),
+		createHtmlPlugin({
+			entry: 'src/main.tsx',
+			template: 'public/index.html',
+			minify: true,
+			inject: {
+				data: {
+					title: 'GitHub Campus Experts India'
+				}
+			}
+		})
+	],
+	base: '/',
+	build: {
+		assetsDir: 'modules',
+		outDir: 'build'
+	}
 });
