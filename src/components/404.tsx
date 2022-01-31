31bf691d9b2ae94fc6af57cddaa6759d01806ea8
Param Siddharth <contact@paramsid.com>
2022-01-31 17:12:11 +0530

    Finish 404 page
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -6,11 +6,13 @@ import {
 import { Link } from 'react-router-dom';
 
 const NotFound = () => <Container fluid='md mt-5 mb-5'>
-	<Row>
+	<Row style={{
+	minHeight: 400
+}} className='align-items-center mt-auto mb-auto'>
 		<Col xs='12' className='text-center'>
 			<h1>Uh oh!</h1>
-			<h3>Looks like you came to the wrong page.</h3>
-			<p>Try going back to the homepage.</p>
+			<h3 className='mb-2'>Looks like you came to the wrong page.</h3>
+			<p className='mb-4'>Try going back to the homepage.</p>
 			<Link to='/' className='btn text-black btn-info'>Return to Home</Link>
 		</Col>
 	</Row>
