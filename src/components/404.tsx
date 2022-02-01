c76396c84c2206b28069a176e0e33eea248643ed
Param Siddharth <contact@paramsid.com>
2022-02-01 23:27:35 +0530

    Add SEO to 404 page
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -5,10 +5,16 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+import Meta from './meta';
+
 const NotFound = () => <Container fluid='md mt-5 mb-5'>
+	<Meta
+		title='Not Found'
+		description='404 - Not Found! This is the official website of GitHub Campus Experts India. Campus Experts are student leaders that strive to build and support diverse and inclusive spaces in technical communities.'
+		/>
 	<Row style={{
-	minHeight: 400
-}} className='align-items-center mt-auto mb-auto'>
+		minHeight: 400
+	}} className='align-items-center mt-auto mb-auto'>
 		<Col xs='12' className='text-center'>
 			<h1>Uh oh!</h1>
 			<h3 className='mb-2'>Looks like you came to the wrong page.</h3>
