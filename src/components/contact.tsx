7a95de65ebb54825a34b925974da5db1de95a42e
Param Siddharth <contact@paramsid.com>
2022-02-07 16:46:15 +0530

    Show 'Loading...' until contact form loads
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import {
 	Container,
 	Row,
@@ -7,45 +8,54 @@ import {
 import { formLink } from '../constants';
 import Meta from './meta';
 
-const Contact = () => <Container style={{
-	minHeight: 400
-}} fluid='md mt-5 mb-5'>
-	<Meta
-		title='Contact Us'
-		description='Contact GitHub Campus Experts India. Campus Experts are student leaders that strive to build and support diverse and inclusive spaces in technical communities.'
-	/>
-	<Row className='justify-content-center mt-auto mb-auto'>
-		<Col xs='12' md='8'>
-			<h4 className='text-center mb-3'>Contact Us</h4>
-		</Col>
-	</Row>
-	<Row className='mt-3 justify-content-center'>
-		<Col xs='12' md='8'>
-			If you:
-			<ul>
-				<li>Seek guidance or mentorship for projects</li>
-				<li>Need help in organizing an event</li>
-				<li>Have anything important to convey</li>
-			</ul>
-			Just fill this form below and let us know!
-		</Col>
-	</Row>
-	<Row className='mt-3 justify-content-center'>
-		<Col xs='12' md='8' className='text-center'>
-			<iframe src={formLink} style={{
-				width: '95%',
-				minHeight: 400,
-				border: '2px solid red',
-				backgroundColor: 'white'
-			}}></iframe>
-		</Col>
-	</Row>
-	<Row className='mt-3 justify-content-center'>
-		<Col xs='12' md='8' className='opacity-50'>
-			Pssst... If you are also interested in becoming a campus expert, apply <a href='https://apply.githubcampus.expert/' target='_blank' rel="noreferrer">here</a>.
-			Applications open twice a year, so hurry up!
-		</Col>
-	</Row>
-</Container>;
+const Contact = () => {
+	const iFrameRef = useRef<HTMLIFrameElement>(null);
+
+	return <Container style={{
+		minHeight: 400
+	}} fluid='md mt-5 mb-5'>
+		<Meta
+			title='Contact Us'
+			description='Contact GitHub Campus Experts India. Campus Experts are student leaders that strive to build and support diverse and inclusive spaces in technical communities.'
+		/>
+		<Row className='justify-content-center mt-auto mb-auto'>
+			<Col xs='12' md='8'>
+				<h4 className='text-center mb-3'>Contact Us</h4>
+			</Col>
+		</Row>
+		<Row className='mt-3 justify-content-center'>
+			<Col xs='12' md='8'>
+				If you:
+				<ul>
+					<li>Seek guidance or mentorship for projects</li>
+					<li>Need help in organizing an event</li>
+					<li>Have anything important to convey</li>
+				</ul>
+				Just fill this form below and let us know!
+			</Col>
+		</Row>
+		<Row className='mt-3 justify-content-center'>
+			<Col xs='12' md='8' className='text-center'>
+				<iframe ref={iFrameRef} srcDoc={`
+					<p style="text-align: center; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+					'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+					sans-serif, Kaithi, Mahajani, 'Meetei Mayek';"><b>Loading...</b></p>
+				`} onLoad={() => iFrameRef.current?.removeAttribute('srcDoc')} src={formLink} style={{
+					width: '95%',
+					minHeight: 400,
+					border: '2px solid red',
+					backgroundColor: 'white',
+					position: 'relative'
+				}}></iframe>
+			</Col>
+		</Row>
+		<Row className='mt-3 justify-content-center'>
+			<Col xs='12' md='8' className='opacity-50'>
+				Pssst... If you are also interested in becoming a campus expert, apply <a href='https://apply.githubcampus.expert/' target='_blank' rel="noreferrer">here</a>.
+				Applications open twice a year, so hurry up!
+			</Col>
+		</Row>
+	</Container>
+};
 
 export default Contact;
